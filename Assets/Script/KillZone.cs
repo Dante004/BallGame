944175a2bbaa71953b5d33e21531c366700e1953
Dante004 <bartek.bk@tlen.pl>
2017-03-07 13:58:35 +0100

    Drobne poprawki, nowa plansza

--- Assets/Script/KillZone.cs
+++ Assets/Script/KillZone.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour {
 
+    public Vector3 spawn;
+
     private void OnTriggerEnter(Collider other)
     {
-        other.transform.position = new Vector3(0, 10, 0); // zmiana pozycji obiektu na domyślną
+        other.transform.position = new Vector3(spawn.x, spawn.y, spawn.z); // zmiana pozycji obiektu na domyślną
     }
 
 }
